ab5da854099ed991037cf103fa0df3fe1f9e9627
aceducky <115702816+aceducky@users.noreply.github.com>
2022-10-23 19:52:38 +0530

    Rock, Paper, Scissors

---
+++ rock_paper_scissors.py
@@ -0,0 +1,46 @@
+import random
+
+
+def rock_paper_scissors():
+    your_score = 0
+    computer_score = 0
+    Win = [
+        ["rock", "scissors"],
+        ["paper", "rock"],
+        ["scissors", "paper"]]
+    while True:
+        computer_choice = random.choice(["rock", "paper", "scissors"])
+        user_choice = input('''Enter one of the choice:
+        (r)rock
+        (p)paper
+        (s)scissors:\t''').lower()
+        if user_choice.strip() == '':
+            print("Thanks for playing......")
+            quit()
+        elif user_choice == 'r':
+            user_choice = 'rock'
+        elif user_choice == 'p':
+            user_choice = 'paper'
+        elif user_choice == 's':
+            user_choice = 'scissors'
+        else:
+            print("Invalid option")
+            quit()
+        choice_list = [user_choice, computer_choice]
+        print(
+            f'User choice: {user_choice},  Computer choice: {computer_choice}')
+        if user_choice == computer_choice:
+            print("\nTie")
+            computer_score += 1
+            your_score += 1
+        elif choice_list in Win:
+            print("You Win")
+            your_score += 1
+        else:
+            print("Computer Wins")
+            computer_score += 1
+        print(f"Your score: {your_score}\nComputer score: {computer_score}")
+        print("\n(Press Enter to exit)\n")
+
+
+rock_paper_scissors()
